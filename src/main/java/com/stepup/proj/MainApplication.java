705655dee3ab5bfd2c7764e8f6a2bf0e9b1e7761
Oleg V V <Olegbbbb@inbox.ru>
2026-02-05 14:46:37 +0200

    Улучшили логику майн и дусмф.

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,13 +4,13 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("111111");
-        System.out.println("222222");
+        System.out.println("------");
     }
 
     public static void doSomth() {
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(0);
+        System.out.println(0);
 
     }
 }
