64c75b72dd123a4b628fb16063a89b69e7f65d82
Oleg V V <Olegbbbb@inbox.ru>
2026-02-05 14:48:17 +0200

    Изменили логику майн и дусмф.

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -2,15 +2,15 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("------");
         System.out.println("111111");
         System.out.println("222222");
     }
 
     public static void doSomth() {
+        System.out.println(0);
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(1);
 
     }
 }
